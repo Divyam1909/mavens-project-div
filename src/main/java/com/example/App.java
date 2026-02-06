eb00867ddc12036d3cd167b988792fcb1eb7c877
NAVIN DIVYAM RITESH <5023134@it.fcrit.ac.in>
2026-02-06 12:51:53 +0530

    second commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,19 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) 
     {
-        System.out.println( "Hello World!" );
+        // You can also hardcode numbers if you want
+        int num1 = 10;
+        int num2 = 25;
+
+        int sum = num1 + num2;
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Sum = " + sum);
     }
 }
